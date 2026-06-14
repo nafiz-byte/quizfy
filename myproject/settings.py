96f712441998a426e8a9ba7b52e7d21069bbd82d
nafiz-byte <nafizuddin1537@gmail.com>
2026-06-14 12:17:18 +0600

    fix csrf trusted origins

--- myproject/settings.py
+++ myproject/settings.py
@@ -72,3 +72,4 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://thinkbattle.up.railway.app']
